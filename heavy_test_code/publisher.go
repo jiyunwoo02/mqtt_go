c11664388ac5474ab430d288b4e0df89248405bf
jiyunwoo <jiyunwoo33@gmail.com>
2024-10-25 18:41:02 +0900

    1025 테스트 프로젝트 진 1일차

--- heavy_test_code/publisher.go
+++ heavy_test_code/publisher.go
@@ -13,33 +13,48 @@ import (
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
-func publishMessages(client mqtt.Client, topic string, message string, n int, qos int, socketConn net.Conn) {
+// 메시지 발행 함수: MQTT 브로커와 소켓을 통해 메시지 전송
+func publishMessages(client mqtt.Client, topic string, message string, n int, qos int, retain bool, socketConn net.Conn) {
+	sendCount := 0 // 소켓으로 전송한 메시지 수를 카운트
 	for i := 1; i <= n; i++ {
 		msg := fmt.Sprintf("%s#%d", message, i) // Hello#1 형식
-		token := client.Publish(topic, byte(qos), false, msg)
+
+		// MQTT 브로커에 메시지 발행
+		token := client.Publish(topic, byte(qos), retain, msg)
 		token.Wait()
 		fmt.Printf("Published: %s\n", msg)
 
 		// 소켓을 통해 메시지 전송
-		_, err := socketConn.Write([]byte(msg + "\n"))
-		if err != nil {
-			log.Printf("Error sending message via socket: %v", err)
+		if socketConn != nil {
+			_, err := socketConn.Write([]byte(msg + "\n")) // writes data to the connection.
+			if err != nil {
+				log.Printf("Error sending message via socket: %v", err)
+			} else {
+				sendCount++ // 소켓으로 메시지가 잘 전달될 때마다 i 1씩 증가
+			}
 		}
 
 		time.Sleep(1 * time.Second) // 발행 간격
 	}
+	if socketConn != nil {
+		fmt.Printf("발행자가 소켓을 통해 총 %d번 메시지를 발송했습니다.\n", sendCount)
+	} // 총 메시지를 몇 번 발송했는지 확인 -> n과 일치해야 함!
+
 	fmt.Println("All messages published.")
 }
 
 func main() {
+	// 명령행 플래그 설정 (플래그명, 기본값, 설명)
 	id := flag.String("id", "publisher1", "The id of the publisher")
-	qos := flag.Int("qos", 0, "QoS level (0, 1, 2)")
-	topic := flag.String("topic", "test/topic", "MQTT topic")
-	n := flag.Int("n", 1, "Number of messages to publish") // n을 입력 받는 걸로 바꾸기
-	address := flag.String("address", "tcp://localhost:1883", "Address of the broker")
+	qos := flag.Int("q", 0, "QoS level (0, 1, 2)")
+	topic := flag.String("tp", "test/topic", "MQTT topic")
+	n := flag.Int("n", 1, "Number of messages to publish")
+	address := flag.String("ad", "tcp://localhost:1883", "Address of the broker")
+	port := flag.String("p", "", "Port to listen for subscriber connections") // 포트 미제공 시 연결 X
 
-	flag.Parse()
+	flag.Parse() // 플래그 파싱
 
+	// MQTT 브로커에 연결 설정
 	opts := mqtt.NewClientOptions().AddBroker(*address).SetClientID(*id)
 	client := mqtt.NewClient(opts)
 	if token := client.Connect(); token.Wait() && token.Error() != nil {
@@ -47,29 +62,39 @@ func main() {
 	}
 	defer client.Disconnect(250)
 
-	// // 사용자 입력 받기
-	// fmt.Print("Enter the message to publish: ")
-	// reader := bufio.NewReader(os.Stdin)
-	// message, _ := reader.ReadString('\n')
-	// message = strings.TrimSpace(message)
+	// 사용자로부터 메시지 입력 받기
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print("Enter the message to publish: ")
+	scanner.Scan()
+	message := scanner.Text()
 
-	// 소켓 서버 설정
-	listener, err := net.Listen("tcp", "localhost:9090") // listen : crate server
-	if err != nil {
-		log.Fatalf("Failed to start socket server: %v", err)
+	// 소켓 서버 설정: 포트(-p)가 제공된 경우에만!
+	var conn net.Conn
+	if *port != "" {
+		listener, err := net.Listen("tcp", "localhost:"+*port)
+		if err != nil {
+			log.Fatalf("Failed to start socket server: %v", err)
+		}
+		defer listener.Close()
+
+		fmt.Printf("Waiting for subscriber connection on port %s...\n", *port)
+		conn, err = listener.Accept()
+		if err != nil {
+			log.Fatalf("Failed to accept subscriber connection: %v", err)
+		}
+		defer conn.Close()
+		fmt.Println("Subscriber connected.")
 	}
-	defer listener.Close()
 
-	fmt.Println("Waiting for subscriber connection...")
-	conn, err := listener.Accept()
-	if err != nil {
-		log.Fatalf("Failed to accept subscriber connection: %v", err)
+	// 메시지 발행 루프: 앞에 r이 있을 경우 retain=True
+	retain := false
+	if strings.HasPrefix(message, "r") {
+		retain = true
+		message = strings.TrimPrefix(message, "r")
 	}
-	fmt.Println("Successed to accept subscriber")
-	defer conn.Close()
 
-	// 메시지 발행 시작
-	// go publishMessages(client, *topic, message, *n, *qos, conn)
+	// 메시지 발행 시작 (MQTT와 소켓 모두로 전송)
+	go publishMessages(client, *topic, strings.TrimSpace(message), *n, *qos, retain, conn)
 
 	select {} // 프로그램이 종료되지 않도록 대기
 }
--- heavy_test_code/subscriber.go
+++ heavy_test_code/subscriber.go
@@ -1,70 +1,47 @@
-// package main
+package main
 
-// import (
-// 	"bufio"
-// 	"fmt"
-// 	"log"
-// 	"net"
-// 	"sync"
-// 	"time"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
 
-// 	mqtt "github.com/eclipse/paho.mqtt.golang"
-// )
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
 
-// func receiveFromSocket(conn net.Conn, id int, expected int, wg *sync.WaitGroup) {
-// 	defer wg.Done()
-// 	scanner := bufio.NewScanner(conn)
-// 	received := 0
+// 각 구독자의 결과를 저장하는 구조체
+type SubscriberResult struct {
+	ID           int
+	Received     int
+	Expected     int
+	IsSuccessful bool
+}
 
-// 	for scanner.Scan() {
-// 		fmt.Printf("[Subscriber %d] Received from socket: %s\n", id, scanner.Text())
-// 		received++
-// 	}
+func main() {
+	// 명령행 플래그 설정 (플래그명, 기본값, 설명)
+	address := flag.String("ad", "tcp://localhost:1883", "Address of the broker")
+	id := flag.String("id", "subscriber1", "The id of the subscriber")
+	topic := flag.String("tp", "test/topic", "MQTT topic")
+	sn := flag.Int("sn", 1, "Number of subscribers")
+	port := flag.String("p", "", "Port to connect for publisher connections")
 
-// 	if received == expected {
-// 		fmt.Printf("[Subscriber %d] Successfully received all messages via socket.\n", id)
-// 	} else {
-// 		fmt.Printf("[Subscriber %d] Missed %d messages via socket.\n", id, expected-received)
-// 	}
-// }
+	// MQTT 클라이언트 설정
+	opts := mqtt.NewClientOptions().AddBroker(*address).SetClientID(*id)
+	client := mqtt.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		log.Fatalf("Failed to connect to broker: %v", token.Error())
+	}
+	defer client.Disconnect(250)
 
-// func subscribeToBroker(client mqtt.Client, topic string, expected int, id int, wg *sync.WaitGroup) {
-// 	defer wg.Done()
-// 	received := 0
-
-// 	client.Subscribe(topic, 0, func(_ mqtt.Client, msg mqtt.Message) {
-// 		fmt.Printf("[Subscriber %d] Received from broker: %s\n", id, msg.Payload())
-// 		received++
-// 	})
-
-// 	time.Sleep(5 * time.Second) // 메시지 수신 대기
-
-// 	fmt.Printf("[Subscriber %d] Received %d/%d messages from broker.\n", id, received, expected)
-// }
-
-// func main() {
-// 	// 소켓 연결
-// 	conn, err := net.Dial("tcp", "localhost:9090") // dial : connect server
-// 	if err != nil {
-// 		log.Fatalf("Failed to connect to publisher via socket: %v", err)
-// 	}
-// 	defer conn.Close()
-
-// 	// MQTT 클라이언트 설정
-// 	opts := mqtt.NewClientOptions().AddBroker("tcp://localhost:1883").SetClientID("subscriber")
-// 	client := mqtt.NewClient(opts)
-// 	if token := client.Connect(); token.Wait() && token.Error() != nil {
-// 		log.Fatalf("Failed to connect to broker: %v", token.Error())
-// 	}
-// 	defer client.Disconnect(250)
-
-// 	var wg sync.WaitGroup
-// 	wg.Add(2)
-
-// 	// 브로커와 소켓에서 메시지 수신 시작
-// 	go receiveFromSocket(conn, 1, 5, &wg) // 기대 메시지 수 5개로 설정
-// 	go subscribeToBroker(client, "test/topic", 5, 1, &wg)
-
-// 	wg.Wait()
-// 	fmt.Println("All messages received.")
-// }
+	// 소켓 연결 (포트가 제공된 경우)
+	var conn net.Conn
+	if *port != "" {
+		var err error
+		conn, err = net.Dial("tcp", "localhost:"+*port)
+		if err != nil {
+			log.Fatalf("Failed to connect to publisher via socket: %v", err)
+		}
+		defer conn.Close()
+		fmt.Println("Connected to publisher via socket.")
+	}
+}
